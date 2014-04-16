ea2a6e5101a34184b694c0aee5687269071649a7
DevSwift <devswift@wp.pl>
2014-04-16 08:19:53 +0200

    typo

--- drivers/gpu/mali/mali400ko/driver/src/devicedrv/mali/common/mali_pp_scheduler.c
+++ drivers/gpu/mali/mali400ko/driver/src/devicedrv/mali/common/mali_pp_scheduler.c
@@ -242,7 +242,7 @@ static void mali_pp_scheduler_schedule(void)
 			}
 		}
 
-+		if (mali_pp_scheduler_balance_jobs) {
+		if (mali_pp_scheduler_balance_jobs) {
  			if ( (0==job->sub_jobs_started) && (num_slots_idle < num_slots) && (job->sub_job_count > num_slots_idle))
  			{
  				MALI_DEBUG_PRINT(4, ("Mali PP scheduler: Job with %d subjobs not started, since only %d/%d cores are available\n", job->sub_job_count, num_slots_idle,num_slots));
